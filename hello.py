078928975bbcf3c8e180244f08713ad2c2b9c2df
Madison Wong <madisonwong@madisons-mbp-2.home>
2023-09-23 14:59:37 -0400

    Modify form to take in email

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -11,9 +11,11 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-class NameForm(FlaskForm):
+class NameEmailForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -27,11 +29,16 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = None
-    form = NameForm()
+    email = None
+    form = NameEmailForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
